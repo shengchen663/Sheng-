c36b27140ccf91ccc5b76bddfcd33f5826bcdf1f
shengchen663 <cs20080321@gmail.com>
2024-12-12 11:31:00 -0500

    Add files via upload

---
+++ aaa3.java
@@ -0,0 +1,18 @@
+package sheng;
+
+import java.util.Scanner;
+
+public class aaa3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 Scanner b = new Scanner(System.in);
+		 System.out.print("Enter the number");
+		 int n = b.nextInt();
+	
+		  for( int i=1;i<=10;i++ ) {
+		int c =(n*i);
+		  System.out.println(n +" * "+ i +" +( 1 * c));
+	}
+
+}
